f6d004903e5b7b276b8000ac633b8fcc73fdabb6
Abhinav Kumar <abhinavbaddu01@gmail.com>
2025-12-09 15:42:21 +0530

    Chg

--- learnVar.js
+++ learnVar.js
@@ -1,132 +1,150 @@
-// // let name = 'abhinav kumar';
-// // var name2 = 'vicky kumar';
-// // const name3 = 'vipul kumar';
+// // // let name = 'abhinav kumar';
+// // // var name2 = 'vicky kumar';
+// // // const name3 = 'vipul kumar';
 
-// const { use } = require("react");
-
-
-// // for(let i = 0;i<name.length;i++){
-// //     console.log(name[i]);
-// // }
-
-// // console.log(name.trim().length);
-// // let name4 = name.trim();
-
-// // console.log(name)
-// // console.log(name4)
-
-// // console.log(name2.toUpperCase())
-// // console.log(name2.toLowerCase())
-
-// // let arr = name3.split(" ");
-// // console.log(arr);
+// // const { use } = require("react");
 
 
-// // let str = "hello world"     //olleh dlrow
-
-// // // let str1 = str.split("");
-// // // console.log(str1)
-// // // let str2 = [];
-// // // for(let i = str1.length-1;i>=0;i--){
-// // //     str2.push(str1[i]);
-// // // }
-// // // console.log(str2.join(''))
-// // // let str2 = str.split(" ");
-// // // console.log(str2)
-// // // let str3 = [];
-// // // for(let i = 0;i<=str2.length-1;i++){
-// // //     let temp = str2[i].split('');
-// // //     temp.push(" ")
-// // //     for(let j = temp.length-1;j>=0;j--){
-// // //         str3.push(temp[j]);
-// // //     }
+// // // for(let i = 0;i<name.length;i++){
+// // //     console.log(name[i]);
 // // // }
 
-// // console.log(str3.join('').trim())
+// // // console.log(name.trim().length);
+// // // let name4 = name.trim();
 
-// // //comment added to check the pushed code in github
-// // const promise1=new Promise((resolve)=>{
-// //         setTimeout(()=>{
-// //             resolve("Done")
-// //         })
-// // })
-// // promise1.then(()=>console.log("First promise create"))
+// // // console.log(name)
+// // // console.log(name4)
 
-// // function sum(a,b,callback){
-// //     console.log('Callback Function')
-// //     callback()
-// // }
-// // function ab(){
-// //     console.log("all About me")
+// // // console.log(name2.toUpperCase())
+// // // console.log(name2.toLowerCase())
+
+// // // let arr = name3.split(" ");
+// // // console.log(arr);
+
+
+// // // let str = "hello world"     //olleh dlrow
+
+// // // // let str1 = str.split("");
+// // // // console.log(str1)
+// // // // let str2 = [];
+// // // // for(let i = str1.length-1;i>=0;i--){
+// // // //     str2.push(str1[i]);
+// // // // }
+// // // // console.log(str2.join(''))
+// // // // let str2 = str.split(" ");
+// // // // console.log(str2)
+// // // // let str3 = [];
+// // // // for(let i = 0;i<=str2.length-1;i++){
+// // // //     let temp = str2[i].split('');
+// // // //     temp.push(" ")
+// // // //     for(let j = temp.length-1;j>=0;j--){
+// // // //         str3.push(temp[j]);
+// // // //     }
+// // // // }
+
+// // // console.log(str3.join('').trim())
+
+// // // //comment added to check the pushed code in github
+// // // const promise1=new Promise((resolve)=>{
+// // //         setTimeout(()=>{
+// // //             resolve("Done")
+// // //         })
+// // // })
+// // // promise1.then(()=>console.log("First promise create"))
+
+// // // function sum(a,b,callback){
+// // //     console.log('Callback Function')
+// // //     callback()
+// // // }
+// // // function ab(){
+// // //     console.log("all About me")
     
 
-// //  }
+// // //  }
  
-// //  sum(34,34,ab)
+// // //  sum(34,34,ab)
  
-// // const users = [{id:1,name:"Abinav"},
-// //     {id:2,name:"Akash"}
-// // ]
-// // const user1=users.reduce((acc,user)=>{
-// //   acc[user.id]=user.name;
-// //   return acc
-// // })
-// // // console.log(user1)
-// // const obj ={
+// // // const users = [{id:1,name:"Abinav"},
+// // //     {id:2,name:"Akash"}
+// // // ]
+// // // const user1=users.reduce((acc,user)=>{
+// // //   acc[user.id]=user.name;
+// // //   return acc
+// // // })
+// // // // console.log(user1)
+// // // const obj ={
+// // //     name:"Abhinav",
+// // //     age:25,
+// // //     lang:"Java"
+// // // }
+// // // for(let value in obj){
+// // // //     console.log(obj.name)
+// // // }
+// // // async function getData() {
+// // //     let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // //     let data =await response.json();
+// // //     console.log(data)
+    
+// // // }
+// // // getData().then(()=>{
+// // //     console.log('Data fetch ')
+// // // })
+// // class User{
+// //     constructor(name,age){
+// //         this.name=name;
+// //         this.age=age
+// //     }
+// // }
+// // let u1= new User("Abhinav",24)
+// // console.log(u1)
+
+// // function User1(name,age){
+// //     return {name,age}
+// // }
+
+// // const u2=  User1("Abhinav",24)
+
+// // const person={
+// //     name:"Akash",
+// //      greet(){
+// //        console.log("Welcome")
+// //     }
+// // }
+// // person.greet()
+
+// // const user ={
 // //     name:"Abhinav",
-// //     age:25,
-// //     lang:"Java"
+// //     greet1(){
+// // console.log(this.name)
+// //     }
 // // }
-// // for(let value in obj){
-// // //     console.log(obj.name)
-// // }
-// // async function getData() {
-// //     let response = await fetch('https://jsonplaceholder.typicode.com/posts');
-// //     let data =await response.json();
-// //     console.log(data)
-    
-// // }
-// // getData().then(()=>{
-// //     console.log('Data fetch ')
-// // })
-// class User{
-//     constructor(name,age){
-//         this.name=name;
-//         this.age=age
-//     }
-// }
-// let u1= new User("Abhinav",24)
-// console.log(u1)
+// // user.greet1()
+// setTimeout(()=>{
+//     console.log("Rahul")
+// })
+// const promise = new Promise((res,rej)=>{
+//     res("Abhi")
+//    setTimeout(()=>{
+//     console.log("Abhinav")
+//    },1000)
+// }).then(()=>{
+//     console.log('Akash')
+// })
 
-// function User1(name,age){
-//     return {name,age}
-// }
+function getUser(){
+    return new Promise(resolve=>
+        setTimeout(()=>console.log("user fetched"),10000)
+    )
+}
+function getpost(){
+    return new Promise(resolve=>
+        setTimeout(()=>console.log("post fetched"),1000)
+    )
+}
+getUser().then(user=> {console.log(user)
+    return getpost();
+}
+)
+.then(post=> console.log(post))
+.catch(err=> console.error('Something error'))
 
-// const u2=  User1("Abhinav",24)
-
-// const person={
-//     name:"Akash",
-//      greet(){
-//        console.log("Welcome")
-//     }
-// }
-// person.greet()
-
-// const user ={
-//     name:"Abhinav",
-//     greet1(){
-// console.log(this.name)
-//     }
-// }
-// user.greet1()
-setTimeout(()=>{
-    console.log("Rahul")
-})
-const promise = new Promise((res,rej)=>{
-    res("Abhi")
-   setTimeout(()=>{
-    console.log("Abhinav")
-   },1000)
-}).then(()=>{
-    console.log('Akash')
-})
